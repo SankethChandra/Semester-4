020bb1fa7b3664819c788fd06e0a2d6fc19c9930
SankethChandra <thatipamula01@gmail.com>
2020-02-06 22:15:50 +0530

    lab

--- DAA/Week-5/q2.c
+++ DAA/Week-5/q2.c
@@ -5,11 +5,14 @@ void taposort(int adj[][100], int v)
 {
     int n = v;
     int val[100];
+
     for (int i = 0; i < v; i++)
         val[i] = i;
+   
     for(int k=0;k<n;k++)
     {
         int del=-1;
+        //checking dag or not and has any node with indegree 0 or not
         for(int i=0;i<v;i++)
         {
             int fl=0;
@@ -23,21 +26,28 @@ void taposort(int adj[][100], int v)
             }
             if(fl==0){
                 del=i;
-                break;
+                break;//break out of loop to continue
             }
         }
+        
+        //checking ends
+        //if not dag
         if(del==-1)
         {
             printf("Sorting not possible\n");
             return;
         }
+
+        //if dag and indegree zero removing the node(i.e., adding it order and removing the node)
         printf("%d\t",val[del]+1);
+        //removing the connections of the node with indegree 0 from adj matrix to continue  
         for(int i=del;i<v-1;i++)
         {
             val[i]=val[i+1];
             for(int j=0;j<v;j++)
                 adj[i][j]=adj[i][j+1];
         }
+
         for(int i=del;i<v-1;i++)
             for(int j=0;j<v;j++)
                 adj[j][i]=adj[j+1][i];
@@ -58,4 +68,4 @@ void main()
     printf("Requiered order\n");
     taposort(adj, v);
     printf("\n");
-}
+}   
--- DAA/Week-5/q3.c
+++ DAA/Week-5/q3.c
@@ -17,14 +17,18 @@ int diameter(nodeptr root)
         return 0;
     int lh=height(root->lchild);
     int rh=height(root->rchild);
-    return max(lh+rh+1,max(lh,rh));
+    int ld=diameter(root->lchild);
+    int rd=diameter(root->rchild);
+    return max(lh+rh+1,max(ld,rd));
 }
+
 void main()
 {
     int x;
     printf("Enter root value\n");
     scanf("%d",&x);
     nodeptr root=createbt(x);
-    inorder(root);
+    //inorder(root);
     printf("Diameter : %d",diameter(root));
+    return;
 }
