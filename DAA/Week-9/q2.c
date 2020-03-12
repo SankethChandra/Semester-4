7eafad5d47883b9716622d1fb5fae143c2a0e5f8
SankethChandra <thatipamula01@gmail.com>
2020-03-12 21:46:59 +0530

    error corrected you dont have to know

--- DAA/Week-9/q2.c
+++ DAA/Week-9/q2.c
@@ -2,7 +2,21 @@
 #include <stdlib.h>
 #include "freefunc.h"
 #include <time.h>
+int opcount;
+int hashsearch(nodeptr hashl,int key)
+{
+	
+	nodeptr temp=hashl;
+	while(temp!=NULL)
+	{
+		opcount++;
+		if(temp->data==key)
+			return 1;
+		else temp=temp->next;
 
+	}
+	return -1;//not found
+}
 void main()
 {
 	int m;
@@ -19,13 +33,25 @@ void main()
 	srand(time(0));
 	//hashit now (also input is generated at random using rand function)
 	//hash function can be changed but the hash value should be in 0-size
-	
+
 	while(count!=n)
 	{	
 		count++;
 		int data=(int)random()%14353;
 		hashtable[data%m]=insert(hashtable[data%m],data);
 	}
+	int key;
 	printf("table\n");
 	PrintTable(hashtable,m);
+	//while(1){
+	printf("\n\nEnter key to search\n");
+	scanf("%d",&key);
+	opcount=0;
+
+	int flag=hashsearch(hashtable[key%m],key);
+	if(flag!=-1)
+		printf("element found opcount %d\n",opcount );
+	else
+		printf("element not found opcount %d\n",opcount);
+	//}
 }
